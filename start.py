a234a9e495b0f119d93a5db70cbd4a80febe2aeb
Mlamlu <mlamlu@hotmail.fr>
2023-08-27 02:26:30 +0200

    add light and send signal

--- start.py
+++ start.py
@@ -1,34 +1,37 @@
-import RPi.GPIO as gp
+import RPi.GPIO as GPIO
 import os
 import time
-from time import sleep   # Gestion du temps
 from datetime import datetime
 from env import raspberry_picture_path
 
-PIN_NUM = 36
+# Numéro des broches GPIO que vous utilisez (numéro de broche physique)
+PIN_RECEIVE = 36
+PIN_SEND = 37
 
-gp.setmode(gp.BOARD)
-gp.setup(PIN_NUM, gp.IN)  # Une entree : le poussoir
+# Configuration de la bibliothèque RPi.GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(PIN_RECEIVE, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(PIN_SEND, GPIO.OUT)
 
-def my_callback(channel):
-    if not gp.input(channel):
-        print("Input Detected")
-        cmd = "python captureCam.py"
-        os.system(cmd)
-
-
-print("Debut du programme")
 isExist = os.path.exists(raspberry_picture_path)
 if not isExist:
     os.mkdir(raspberry_picture_path)
-    
-print("Vous pouvez aussi terminer avec CTRL+C \n")
-gp.add_event_detect(PIN_NUM, gp.FALLING, callback=my_callback, bouncetime=1000)
+
+def detect_signal(channel):
+    if GPIO.input(PIN_RECEIVE) == GPIO.HIGH:
+        print("Signal détecté!")
+        cmd = "python captureCam.py"
+        os.system(cmd)
+
+GPIO.add_event_detect(PIN_RECEIVE, GPIO.RISING, callback=detect_signal, bouncetime=75)
 
 try:
-    print("Attente de courant électrique...")
     while True:
-        pass  # Le programme continue à s'exécuter en arrière-plan
+        print("send signal...")
+        GPIO.output(PIN_SEND, GPIO.HIGH)  # Envoyer le signal en niveau haut
+        time.sleep(1)  # Attendre pendant 0.5 seconde
+        GPIO.output(PIN_SEND, GPIO.LOW)   # Envoyer le signal en niveau bas
+        time.sleep(10.5)  # Attendre pendant 4.5 secondes (total 5 secondes)
 
 except KeyboardInterrupt:
-    gp.cleanup()  # Nettoyage des configurations GPIO en cas d'interruption
+    GPIO.cleanup()  # Nettoyage des configurations GPIO en cas d'interruption
